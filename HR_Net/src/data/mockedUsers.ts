245808db1082597b5c7048a66e1be300ae66bee2
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-02-14 16:45:21 +0100

    data

---
+++ HR_Net/src/data/mockedUsers.ts
@@ -0,0 +1,10 @@
+import MOCK_DATA from './MOCK_DATA.json'
+
+export function createMockedUsers() {
+    const mockedUsers: Array<object> = []
+
+    MOCK_DATA.forEach((item) => mockedUsers.push(item))
+    console.log(mockedUsers)
+
+    return mockedUsers
+}
