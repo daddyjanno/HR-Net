250641ada815ac777b08166f3fc5c7e14afcf909
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-02-14 16:47:33 +0100

    delete layout

--- HR_Net/src/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer">
-            <p className="footer-text">Copyright 2024 HR Net</p>
-        </footer>
-    )
-}
-
-export default Footer
